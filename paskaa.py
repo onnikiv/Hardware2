613c2209982afef01444b04cde77f887c0baee6c
onnik <onni.kivinen@gmail.com>
2024-11-14 13:52:34 +0200

    assd

--- paskaa.py
+++ paskaa.py
@@ -7,12 +7,7 @@ i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
 oled_width = 128
 oled_height = 64
 oled = SSD1306_I2C(oled_width, oled_height, i2c)
-
-LEDS = {
-    1: "LED1",
-    2: "LED2",
-    3: "LED3"
-}
+led = Pin(22, mode = Pin.OUT)
 
 class Encoder:
     def __init__(self, rot_a, rot_b, button):
@@ -21,6 +16,8 @@ class Encoder:
         self.button = Pin(button, mode=Pin.IN, pull=Pin.PULL_UP)
         self.fifo = Fifo(30, typecode='i')
         self.a.irq(handler=self.handler, trigger=Pin.IRQ_RISING, hard=True)
+        self.button.irq(handler = self.button_handler, trigger = Pin.IRQ_FALLING, hard = True)
+
     
     def handler(self, pin):
         if self.b():
@@ -28,22 +25,60 @@ class Encoder:
         else:
             self.fifo.put(1)
 
-rot = Encoder(10, 11, 12)
-y = 1
+    def button_handler(self,pin):
+        self.fifo.put(2)
 
+    
+
+class LedScreen:
+    
+    def __init__(self):
+        self.y_axl = 0
+        self.leds = {
+            " LED1": 'OFF',
+            " LED2": 'OFF',
+            " LED3": 'OFF'}
+        self.state = self.off
+        
+    def off(self):
+        oled.fill(0)
+        y = 0
+        for key, value in sorted(self.leds.items()): #perkele
+            oled.text(f"{key}: {value}", 0, y)
+            y += 10
+        oled.show()
+        self.state = self.cursor
+    
+    def cursor(self):
+        
+        self.y_axl += rot.fifo.get()
+        
+        if self.y_axl > 1:
+            self.y_axl = 1
+        elif self.y_axl < -1:
+            self.y_axl = -1
+
+        print(f"y value: {self.y_axl}")
+
+        oled.fill(0)
+        for i, key in enumerate(sorted(self.leds.keys())):
+            if i - 1 == self.y_axl:
+                oled.text(f"{key}: {self.leds[key]}<", 0, 10 * i, 1)
+            else:
+                oled.text(f"{key}: {self.leds[key]}", 0, 10 * i, 1)
+        oled.show()
+        
+        
+        
+
+
+
+rot = Encoder(10, 11, 12)
+led_screen = LedScreen()
+
+rot.fifo.put(1)
 while True:
     if rot.fifo.has_data():
-        y += rot.fifo.get()
-        if y < 1:
-            y = 3
-        elif y > 3:
-            y = 1
+        led_screen.state()
         
-        oled.fill(0)
-        for i in range(1, 4):
-            if i == y:
-                oled.text(f">{LEDS[i]}<", 0, 10 * i, 1)
-            else:
-                oled.text(f" {LEDS[i]}", 0, 10 * i, 1)
-        oled.show()
 
