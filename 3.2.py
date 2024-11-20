63b4fb9ee2c5f32429f4c493f594324b85b50507
onnik <onni.kivinen@gmail.com>
2024-11-20 13:54:30 +0200

    tweaks

--- 3.2.py
+++ 3.2.py
@@ -1,8 +1,8 @@
-from machine import Pin
+from machine import Pin, I2C
 from fifo import Fifo
-from machine import I2C
 from ssd1306 import SSD1306_I2C
 import time
+
 
 class Encoder:
     def __init__(self, rot_a, rot_b):
@@ -19,99 +19,60 @@ class Encoder:
 
 
 class LedMenu:
-    def __init__(self, button):
+    def __init__(self, button, led1, led2, led3):
         self.i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
-        self.oled_width = 128
-        self.oled_height = 64
-        self.oled = SSD1306_I2C(self.oled_width, self.oled_height, self.i2c)
+        self.oled = SSD1306_I2C(128, 64, self.i2c)
         self.button = Pin(button, mode=Pin.IN, pull=Pin.PULL_UP)
         self.button.irq(handler=self.button_handler, trigger=Pin.IRQ_FALLING, hard=True)
         self.fifo = Fifo(10, typecode="i")
-        
         self.old_time = 0
-        
-        self.led1 = Pin(22, Pin.OUT)
-        self.led2 = Pin(21, Pin.OUT)
-        self.led3 = Pin(20, Pin.OUT)
-        self.leds = {
-            self.led1: 'OFF',
-            self.led2: 'OFF',
-            self.led3: 'OFF'
-        }
-        
-        for led in self.leds.keys():
+
+        self.leds = [Pin(led1, Pin.OUT), Pin(led2, Pin.OUT), Pin(led3, Pin.OUT)]
+        self.led_states = ['OFF', 'OFF', 'OFF']
+
+        for led in self.leds:
             led.off()
 
-        self.current_row = 1
+        self.current_row = 0
         self.state = self.cursor
-
 
     def button_handler(self, pin):
         delay = 50
         current_time = time.ticks_ms()
-        if current_time - self.old_time < delay:
-            return
-        self.fifo.put(2)
-        self.old_time = current_time
+        
+        if time.ticks_diff(current_time, self.old_time) >= delay:
+            self.fifo.put(2)
+            self.old_time = current_time
 
+
+    def update_oled_display(self):
+        self.oled.fill(0)
+        for i, state in enumerate(self.led_states):
+            led_name = f"LED{i + 1}"
+            suffix = " <" if i == self.current_row else ""
+            self.oled.text(f"{led_name}: {state}{suffix}", 0, 10 * (i + 1), 1)
+        self.oled.show()
 
     def cursor(self):
-        self.oled.fill(0)
+        if not rot.fifo.has_data():
+            return
         movement = rot.fifo.get()
-        
-        if movement == -1 and self.current_row < 2:
+        if movement == -1 and self.current_row < len(self.leds) -1 : 
             self.current_row += 1
         elif movement == 1 and self.current_row > 0:
             self.current_row -= 1
-
-
-        for i, (led, status) in enumerate(self.leds.items()):
-            led_name = f"LED{i + 1}"
-            if i == self.current_row:
-                self.oled.text(f"{led_name}: {status} <", 0, 10 * (i + 1), 1)
-            else:
-                self.oled.text(f"{led_name}: {status}", 0, 10 * (i + 1), 1)
-
-        self.oled.show()
-
-
-    def menu_update(self):
-        self.oled.fill(0)
-        for i, (led, status) in enumerate((self.leds.items())):
-            led_name = f"LED{i + 1}"
-            if i == self.current_row:
-
-                self.oled.text(f"{led_name}: {status} <", 0, 10 * (i + 1), 1)
-            else:
-                self.oled.text(f"{led_name}: {status}", 0, 10 * (i + 1), 1)
-
-        self.oled.show()
-        self.state = self.cursor
-
+        self.update_oled_display()
 
     def led_toggle(self):
-
-            action = self.fifo.get()
-            if action == 2:
-                selected_led = list(self.leds.keys())[self.current_row]
-
-
-                if selected_led == self.led1:
-                    self.led1.toggle() 
-                    self.leds[self.led1] = 'ON' if self.led1.value() else 'OFF'
-                elif selected_led == self.led2:
-                    self.led2.toggle()
-                    self.leds[self.led2] = 'ON' if self.led2.value() else 'OFF'
-                elif selected_led == self.led3:
-                    self.led3.toggle()
-                    self.leds[self.led3] = 'ON' if self.led3.value() else 'OFF'
-
-
-                self.menu_update()
+        if self.fifo.has_data() and self.fifo.get() == 2:
+            selected_led = self.leds[self.current_row]
+            selected_led.toggle()
+            self.led_states[self.current_row] = 'ON' if selected_led.value() else 'OFF'
+            self.update_oled_display()
 
 
 rot = Encoder(10, 11)
-led_screen = LedMenu(12)
+led_screen = LedMenu(12,22,21,20)
 
 while True:
     if rot.fifo.has_data():
